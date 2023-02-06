8095bab58efb0b090999a2010175e0f74f3772ea
sheyax <olusheyax@gmail.com>
2023-02-06 09:54:59 +0000

    cors erros

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ require("./db");
 const cors = require("cors");
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://o2j6sr-3000.preview.csb.app",
-  })
-);
+const corsOptions = {
+  origin: "https://o2j6sr-3000.preview.csb.app/",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 app.use(bodyParser.json());
 
@@ -17,9 +18,8 @@ var datafeed = require("./controllers/datafeed");
 
 app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
 
+app.use("/auth", authentication);
+app.use("/feed", datafeed);
 const port = 5000;
 
 app.listen(port);
-
-app.use("/auth", authentication);
-app.use("/feed", datafeed);
